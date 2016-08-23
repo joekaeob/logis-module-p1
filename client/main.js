1cf10e9d2f57b0da154c9735bd28cdd126a3f86e
jko <production.jko@gmail.com>
2016-08-23 17:32:08 +0700

    fourth commit

--- client/main.js
+++ client/main.js
@@ -3,7 +3,6 @@ import angularMeteor from 'angular-meteor';
 import gateMonitor from '../imports/components/gateMonitor/gateMonitor';
 import itemManagement from '../imports/components/itemManagement/itemManagement';
 
- 
 angular.module('main-module', [
   angularMeteor,
   //gateMonitor.name
